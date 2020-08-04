884ddc34a500b7d812afed0e6e06bd5608b048b1
invisikami <18693838+invisikami@users.noreply.github.com>
2020-08-04 11:53:46 -0700

    rev

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const right = document.querySelector("#btn-right");
 const left = document.querySelector("#btn-left");
 const img = document.querySelector("#img-container");
 const attr = document.querySelector("#attr");
+
+
 // const images = [
 //     "amy-shamblen-koYklSUjAXc-unsplash.jpg",
 //     "catrin-johnson-ym96FAhQ8o4-unsplash.jpg",
@@ -44,29 +46,26 @@ function decreaseIndex() {
     index = (index < 0) ? (images.length - 1) : index;
 }
 
-// =======================================================
+//========================================================
+
 function init() {
     // Create a request variable and assign a new XMLHttpRequest object to it.
     var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.unsplash.com/search/photos/?query=yellow&per_page=25utm_source=demo&utm_medium=referral ', true);
-    request.setRequestHeader('Authorization', 'Client-ID UG-YxcUndp6I6lLO3o-OO-IsYkX7KjcpvHHkxQcQxFs');
-    // Open a new connection, using the GET request on the URL endpoint
-    // request.open('GET', 'https://api.unsplash.com/photos/?client_id=UG-YxcUndp6I6lLO3o-OO-IsYkX7KjcpvHHkxQcQxFs', true)
-
+    request.open('GET', 'http://api-relay-dwb.herokuapp.com/api/search', true);
 
     request.onload = function() {
         // Begin accessing JSON data here
         let data = JSON.parse(this.response);
         console.log(data);
         if (request.status >= 200 && request.status < 400) {
-            data.results.forEach((pics) => {
+            data.images.forEach((pics) => {
                 let image = new Object();
-                image.user = pics.user.name;
-                image.username = pics.user.username;
-                image.url = pics.urls.regular;
+                image.user = pics.user;
+                image.username = pics.username;
+                image.url = pics.url;
                 images.push(image);
-                // images.push(pics.urls.regular);
-            })
+
+            });
         } else {
             console.log('error');
             console.log(data.errors);
@@ -81,8 +80,50 @@ function init() {
         attr.innerHTML = `Photo by <a href = "https://unsplash.com/@${images[index].username}?utm_source=demo&utm_medium=referral"> ${images[index].user} </a> on <a href="https://unsplash.com/?utm_source=demo&utm_medium=referral ">Unsplash</a>`;
     });
 }
+
+
+// =======================================================
+// function init() {
+//     // Create a request variable and assign a new XMLHttpRequest object to it.
+//     var request = new XMLHttpRequest();
+//     request.open('GET', 'https://api.unsplash.com/search/photos/?query=blonde&per_page=100utm_source=demo&utm_medium=referral ', true);
+//     request.setRequestHeader('Authorization', 'Client-ID UG-YxcUndp6I6lLO3o-OO-IsYkX7KjcpvHHkxQcQxFs');
+//     // Open a new connection, using the GET request on the URL endpoint
+//     // request.open('GET', 'https://api.unsplash.com/photos/?client_id=UG-YxcUndp6I6lLO3o-OO-IsYkX7KjcpvHHkxQcQxFs', true)
+
+
+//     request.onload = function() {
+//         // Begin accessing JSON data here
+//         let data = JSON.parse(this.response);
+//         console.log(data);
+//         if (request.status >= 200 && request.status < 400) {
+//             data.results.forEach((pics) => {
+//                 let image = new Object();
+//                 image.user = pics.user.name;
+//                 image.username = pics.user.username;
+//                 image.url = pics.urls.regular;
+//                 images.push(image);
+
+//             });
+//         } else {
+//             console.log('error');
+//             console.log(data.errors);
+//         }
+//     }
+
+//     // Send request
+//     request.send();
+
+//     request.addEventListener('loadend', () => {
+//         img.style.backgroundImage = getImage();
+//         attr.innerHTML = `Photo by <a href = "https://unsplash.com/@${images[index].username}?utm_source=demo&utm_medium=referral"> ${images[index].user} </a> on <a href="https://unsplash.com/?utm_source=demo&utm_medium=referral ">Unsplash</a>`;
+//     });
+// }
 //how to connect to an api
 //https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
 
 // env variables
-// https: //www.robertcooper.me/front-end-javascript-environment-variables
+// 
+
+
+// https: //www.impressivewebs.com/npm-for-beginners-a-guide-for-front-end-developers/
